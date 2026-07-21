847763b93c2c0209ced7c7fbebd93b0b8174cff4
gimhana-io <gimhanad@gmail.com>
2026-07-21 16:53:02 +0530

    project setup

---
+++ src/main/java/com/talentgrid/app/TalentgridAppApplication.java
@@ -0,0 +1,13 @@
+package com.talentgrid.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalentgridAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalentgridAppApplication.class, args);
+	}
+
+}
