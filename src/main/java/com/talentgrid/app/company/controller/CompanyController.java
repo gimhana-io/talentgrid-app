908b8e1f2d66801cd51993cdae52b96da6fe6ceb
gimhana-io <gimhanad@gmail.com>
2026-07-21 18:02:38 +0530

    add versioning

---
+++ src/main/java/com/talentgrid/app/company/controller/CompanyController.java
@@ -0,0 +1,17 @@
+package com.talentgrid.app.company.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/companies")
+public class CompanyController {
+
+    @GetMapping(version = "1.0")
+    public ResponseEntity<String> getCompany() {
+        return ResponseEntity.ok("Company details");
+    }
+    
+}
