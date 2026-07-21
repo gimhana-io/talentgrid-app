db340b1c3165018f03f5f606015030d7a2a5dba2
gimhana-io <gimhanad@gmail.com>
2026-07-21 18:13:10 +0530

    bug fix

--- src/main/java/com/talentgrid/app/config/web/WebConfig.java
+++ src/main/java/com/talentgrid/app/config/web/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void configureApiVersioning(ApiVersionConfigurer configurer) {
         
-        configurer.useMediaTypeParameter(MediaType.parseMediaType("application/vnd.talentgrid+json"), "v").addSupportedVersions("1.0","2.0","3.0").setDefaultVersion(("1.0"));
+        configurer.useMediaTypeParameter(MediaType.parseMediaType("application/vnd.talentgrid+json"), "v").addSupportedVersions("1.0","2.0","3.0").setDefaultVersion("1.0");
 
     }
 
