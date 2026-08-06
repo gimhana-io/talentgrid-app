7b71302f2b4389c702b14dd800ffb0520db50db7
gimhana-io <gimhanad@gmail.com>
2026-08-07 05:01:07 +0530

    create global exception handler

---
+++ src/main/java/com/talentgrid/app/dto/ErrorResponseDto.java
@@ -0,0 +1,9 @@
+package com.talentgrid.app.dto;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+
+public record ErrorResponseDto(String apiPath, HttpStatus errorCode, String errorMessage, LocalDateTime errorTime) {
+    
+}
---
+++ src/main/java/com/talentgrid/app/exception/GlobalExceptionHandler.java
@@ -0,0 +1,39 @@
+package com.talentgrid.app.exception;
+
+import java.time.LocalDateTime;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+
+import com.talentgrid.app.dto.ErrorResponseDto;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(Exception.class)    
+    public ResponseEntity<ErrorResponseDto> handleException(Exception exception, WebRequest webRequest) {
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(
+                webRequest.getDescription(false),
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                exception.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(errorResponseDto, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+      @ExceptionHandler(NullPointerException.class)    
+    public ResponseEntity<ErrorResponseDto> handleNullException(Exception exception, WebRequest webRequest) {
+        ErrorResponseDto errorResponseDto = new ErrorResponseDto(
+                webRequest.getDescription(false),
+                HttpStatus.INTERNAL_SERVER_ERROR,
+                "A Null Pointer Exception occurred: " + exception.getMessage(),
+                LocalDateTime.now()
+        );
+        return new ResponseEntity<>(errorResponseDto, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  
+    
+}
