5bd82e9a8609d0d9f1e37f6bb5ef5f6bb43d2f22
gimhana-io <gimhanad@gmail.com>
2026-08-06 05:47:55 +0530

    create contact, controller and service

--- src/main/java/com/talentgrid/app/contact/service/impl/ContactServiceImpl.java
+++ src/main/java/com/talentgrid/app/contact/service/impl/ContactServiceImpl.java
@@ -1,16 +1,38 @@
 package com.talentgrid.app.contact.service.impl;
 
+import java.time.Instant;
+
+import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
 import com.talentgrid.app.contact.service.IContactService;
 import com.talentgrid.app.dto.ContactRequestDto;
+import com.talentgrid.app.entity.Contact;
+import com.talentgrid.app.repository.ContactRepository;
+
 
 @Service
 public class ContactServiceImpl implements IContactService {
 
+    private final ContactRepository contactRepository = null;
+
     @Override
     public boolean saveContact(ContactRequestDto contactRequestDto) {
-        return false;
+        boolean result = false;
+        Contact contact = contactRepository.save(transformToEntity(contactRequestDto));
+        if(contact != null && contact.getId() != null) {
+            result = true;
+        }
+        return result;
+    }
+
+    private Contact transformToEntity(ContactRequestDto contactRequestDto) {
+        Contact contact = new Contact();
+        BeanUtils.copyProperties(contactRequestDto, contact);
+
+        contact.setCreatedAt(Instant.now());
+        contact.setCreatedBy("System");
+        return contact;
     }
 
 }
