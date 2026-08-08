c0382473e602603ecf0398760a2f9c061aa0fca4
gimhana-io <gimhanad@gmail.com>
2026-08-08 07:23:05 +0530

    change GlobalExceptionHandler to respond with meaningful messages

--- src/main/java/com/talentgrid/app/exception/GlobalExceptionHandler.java
+++ src/main/java/com/talentgrid/app/exception/GlobalExceptionHandler.java
@@ -1,9 +1,14 @@
 package com.talentgrid.app.exception;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
@@ -22,6 +27,15 @@ public class GlobalExceptionHandler {
                 LocalDateTime.now()
         );
         return new ResponseEntity<>(errorResponseDto, HttpStatus.INTERNAL_SERVER_ERROR);
+    }  
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)    
+    public ResponseEntity<Map<String, String>> handleException(MethodArgumentNotValidException exception) {
+        Map<String, String> errors = new HashMap<>();
+        List<FieldError> fieldErrorList = exception.getBindingResult().getFieldErrors();
+        fieldErrorList.forEach(fieldError -> errors.put(fieldError.getField(), fieldError.getDefaultMessage()));
+        return ResponseEntity.badRequest().body(errors);
+        
     }  
     
 }
