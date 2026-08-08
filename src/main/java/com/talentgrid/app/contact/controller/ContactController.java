bdb8184219a09dfeae5e6fd405e6bf20ed7c24b8
gimhana-io <gimhanad@gmail.com>
2026-08-08 07:55:14 +0530

    add query param validation

--- src/main/java/com/talentgrid/app/contact/controller/ContactController.java
+++ src/main/java/com/talentgrid/app/contact/controller/ContactController.java
@@ -2,15 +2,20 @@ package com.talentgrid.app.contact.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.talentgrid.app.contact.service.IContactService;
 import com.talentgrid.app.dto.ContactRequestDto;
 
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 
 
@@ -29,5 +34,14 @@ public class ContactController {
         } else {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Failed to save contact message.");
         }
+    }
+
+    @GetMapping(version="1.0")
+    public ResponseEntity<String> fetchOpenContacts(@RequestParam 
+        @Validated 
+        @Size(min = 4, message = "Status length should be of minium 4 characters")
+        @NotBlank(message = "Status cannot be blank") 
+     String status){
+        return ResponseEntity.ok("These are the contacts with the give status: " + status);
     }
 }
--- src/main/java/com/talentgrid/app/exception/GlobalExceptionHandler.java
+++ src/main/java/com/talentgrid/app/exception/GlobalExceptionHandler.java
@@ -4,14 +4,17 @@ import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
+import org.springframework.validation.method.ParameterValidationResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.method.annotation.HandlerMethodValidationException;
 
 import com.talentgrid.app.dto.ErrorResponseDto;
 
@@ -28,6 +31,24 @@ public class GlobalExceptionHandler {
         );
         return new ResponseEntity<>(errorResponseDto, HttpStatus.INTERNAL_SERVER_ERROR);
     }  
+
+    @ExceptionHandler(HandlerMethodValidationException.class)
+    public ResponseEntity<Map<String,String>> handleException(HandlerMethodValidationException exception) {
+        Map<String, String> errors = new HashMap<>();
+        List<ParameterValidationResult> results = exception.getParameterValidationResults();
+        results.forEach(result -> {
+            String paramName = result.getMethodParameter().getParameterName();
+
+            // Combine all messages into a single comma-separated string
+            String combinedMessages = result.getResolvableErrors()
+                    .stream()
+                    .map(error -> error.getDefaultMessage())  // extract each message
+                    .collect(Collectors.joining(", "));       // join messages
+            errors.put(paramName, combinedMessages);
+        });
+        return ResponseEntity.badRequest().body(errors);
+    }
+
 
     @ExceptionHandler(MethodArgumentNotValidException.class)    
     public ResponseEntity<Map<String, String>> handleException(MethodArgumentNotValidException exception) {
