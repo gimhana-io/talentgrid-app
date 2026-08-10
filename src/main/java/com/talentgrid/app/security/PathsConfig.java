624ab2d7522e9852c24246fc9d521d5bb396a9ed
gimhana-io <gimhanad@gmail.com>
2026-08-10 15:38:33 +0530

    create custom security configurations

---
+++ src/main/java/com/talentgrid/app/security/PathsConfig.java
@@ -0,0 +1,30 @@
+package com.talentgrid.app.security;
+
+import java.util.List;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class PathsConfig {
+    @Bean(name = "publicPaths")
+    public List<String> publicPaths() {
+        return List.of(
+                "/api/companies/public",
+                "/api/contacts/public",
+                "/api/swagger-ui.html",
+                "/swagger-ui/**",
+                "/api/v3/api-docs/**",
+                "/swagger-resources/**",
+                "/swagger-ui.html",
+                "/webjars/**"
+        );
+    }
+
+    @Bean(name = "securedPaths")
+    public List<String> securedPaths() {
+        return List.of(
+                "/api/**"
+        );
+    }
+}
--- src/main/java/com/talentgrid/app/security/TalentgridSecurityConfig.java
+++ src/main/java/com/talentgrid/app/security/TalentgridSecurityConfig.java
@@ -2,24 +2,39 @@ package com.talentgrid.app.security;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.RegexRequestMatcher;
+
+import lombok.RequiredArgsConstructor;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class TalentgridSecurityConfig {
+
+    @Qualifier("publicPaths")
+    private final List<String> publicPaths;
+
+    @Qualifier("securedPaths")
+    private final List<String> securedPaths;
 
     @Bean
     SecurityFilterChain customSecurityfilterChain(HttpSecurity http) 
     {
         return http
         .csrf((csrfConfig) -> csrfConfig.disable())
-        .authorizeHttpRequests((requests) -> 
-            requests.requestMatchers(RegexRequestMatcher.regexMatcher(".*public$")).permitAll())
+        .authorizeHttpRequests(requests -> 
+        {
+            publicPaths.forEach(path -> requests.requestMatchers(path).permitAll());
+            securedPaths.forEach(path -> requests.requestMatchers(path).authenticated());
+            requests.anyRequest().denyAll();
+        })
         .formLogin((flc) -> flc.disable())
         .httpBasic(withDefaults())
         .build();
