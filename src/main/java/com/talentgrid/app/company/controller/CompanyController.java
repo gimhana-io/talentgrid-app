8b7fb294554b9239798ee6f21abbe1574f4849aa
gimhana-io <gimhanad@gmail.com>
2026-08-12 13:36:54 +0530

    save log messages to a file

--- src/main/java/com/talentgrid/app/company/controller/CompanyController.java
+++ src/main/java/com/talentgrid/app/company/controller/CompanyController.java
@@ -11,10 +11,12 @@ import com.talentgrid.app.company.service.ICompanyService;
 import com.talentgrid.app.dto.CompanyDto;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @RequestMapping("/companies")
 @RequiredArgsConstructor
+@Slf4j
 public class CompanyController {
 
     private final ICompanyService companyService;
@@ -22,6 +24,11 @@ public class CompanyController {
   
     @GetMapping(path="/public", version = "1.0")
     public ResponseEntity<List<CompanyDto>> getAllCompanies() {
+        log.trace("TRACE: this is a trace message");
+        log.debug("DEBUG: this is a debug message");
+        log.info("INFO: this is an info message");
+        log.warn("WARN: this is a warning message");
+        log.error("ERROR: this is an error message");
         List<CompanyDto> companies = companyService.getAllCompanies();
         return ResponseEntity.ok(companies);
     }
