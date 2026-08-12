a9113aaf7877e9671eaba5626d9a8de1b769ee46
gimhana-io <gimhanad@gmail.com>
2026-08-12 11:50:44 +0530

    implement CSRF protection

---
+++ src/main/java/com/talentgrid/app/security/CsrfController.java
@@ -0,0 +1,23 @@
+package com.talentgrid.app.security;
+
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+
+@RestController
+@RequestMapping("/csrf-token")
+public class CsrfController {
+
+
+    @GetMapping(path = "/public", version="1.0")
+    public CsrfToken csrfToken(HttpServletRequest request){
+        return (CsrfToken) request.getAttribute(CsrfToken.class.getName());
+    }
+
+    
+   
+}
--- src/main/java/com/talentgrid/app/security/TalentgridSecurityConfig.java
+++ src/main/java/com/talentgrid/app/security/TalentgridSecurityConfig.java
@@ -20,6 +20,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.password.HaveIBeenPwnedRestApiPasswordChecker;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -44,7 +46,9 @@ public class TalentgridSecurityConfig {
     SecurityFilterChain customSecurityfilterChain(HttpSecurity http) 
     {
         return http
-        .csrf((csrfConfig) -> csrfConfig.disable())
+        .csrf((csrfConfig) -> csrfConfig
+        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+        .csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler()))
         .cors(corsConfig -> corsConfig.configurationSource(corsConfigurationSource()))
         .authorizeHttpRequests(requests -> 
         {
