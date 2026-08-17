1298b84e8075e5d70b1e47611395d5a6a42fbe91
gimhana-io <gimhanad@gmail.com>
2026-08-17 14:00:46 +0530

    Introduce Caffeine library and configure

--- src/main/java/com/talentgrid/app/repository/CompanyRepository.java
+++ src/main/java/com/talentgrid/app/repository/CompanyRepository.java
@@ -3,6 +3,8 @@ package com.talentgrid.app.repository;
 import java.math.BigDecimal;
 import java.util.List;
 
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -12,9 +14,17 @@ import com.talentgrid.app.entity.Company;
 
 public interface CompanyRepository extends JpaRepository<Company, Long> {
 
+    @Cacheable("jobs")
     @Query("SELECT DISTINCT c FROM Company c JOIN FETCH c.jobs j WHERE j.status = :status")
     List<Company> findAllWithJobsByStatus(@Param("status") String status);
 
+    @CacheEvict(value = "companies", allEntries = true)
+    void deleteById(Long id);
+
+    @CacheEvict(value = "companies", allEntries = true)
+    Company save(Company entity);
+
+    @CacheEvict(value = "companies", allEntries = true)
     @Modifying(clearAutomatically = true, flushAutomatically = true)
     int updateCompanyDetails(
             @Param("id") Long id,
