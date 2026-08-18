a4d7225fc457d3efbb9d8cf9afcdad7edd6cc642
gimhana-io <gimhanad@gmail.com>
2026-08-19 04:23:20 +0530

    disable auth in non prod enviornments

--- src/main/java/com/talentgrid/app/security/TalentgridNonProdUsernamePwdAuthenticationProvider.java
+++ src/main/java/com/talentgrid/app/security/TalentgridNonProdUsernamePwdAuthenticationProvider.java
@@ -5,13 +5,11 @@ import java.util.List;
 import org.jspecify.annotations.Nullable;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.authentication.AuthenticationProvider;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import com.talentgrid.app.entity.TalentgridUser;
@@ -19,19 +17,16 @@ import com.talentgrid.app.repository.TalentgridUserRepository;
 
 import lombok.RequiredArgsConstructor;
 
-@Profile("prod")
+@Profile("!prod")
 @Component
 @RequiredArgsConstructor
-public class TalentgridUsernamePwdAuthenticationProvider implements AuthenticationProvider{
+public class TalentgridNonProdUsernamePwdAuthenticationProvider implements AuthenticationProvider{
     
-
     private final TalentgridUserRepository talentgridUserRepository;
-    private final PasswordEncoder passwordEncoder;
 
     @Override
     public @Nullable Authentication authenticate(Authentication authentication) throws AuthenticationException {
        String username = authentication.getName();
-       String pwd = authentication.getCredentials().toString();
        TalentgridUser talentgridUser = talentgridUserRepository.findTalentgridUserByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException(
                         "User details not found for the user: " + username)
@@ -39,11 +34,8 @@ public class TalentgridUsernamePwdAuthenticationProvider implements Authenticati
         
         List<SimpleGrantedAuthority> authorities = List.of(
                 new SimpleGrantedAuthority(talentgridUser.getRole().getName()));
-        if (passwordEncoder.matches(pwd, talentgridUser.getPasswordHash())) {
             return new UsernamePasswordAuthenticationToken(talentgridUser, null, authorities);
-        } else {
-            throw new BadCredentialsException("Invalid password!");
-        }
+       
     }
 
     @Override
